b12008f324ee6df825bcdb72649c4cbc0f648df0
Rastaaslan <damien.lagorce@gmail.com>
2025-03-14 18:37:21 +0100

    correction

--- rastaaslan_website/settings.py
+++ rastaaslan_website/settings.py
@@ -133,7 +133,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     '/var/www/html/rastaaslan_app/static',
 ]
-STATIC_ROOT = '/var/www/html/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
