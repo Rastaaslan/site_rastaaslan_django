3250492d66a40f66116807547840648eaf51a1e3
Rastaaslan <damien.lagorce@gmail.com>
2025-03-14 16:46:02 +0100

    correction

--- rastaaslan_website/settings.py
+++ rastaaslan_website/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = '/var/www/html/static/'
+STATIC_ROOT = '/var/www/html/rastaaslan_app/static/'
 STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
